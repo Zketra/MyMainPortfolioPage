c793171346ff5fdcda526ba69737964df354af2f
zketra <ybdemirbag@gmail.com>
2025-01-12 22:34:45 +0100

    Final Commit before deployement

--- src/components/Skills/Skills.vue
+++ src/components/Skills/Skills.vue
@@ -39,25 +39,25 @@ export default defineComponent({
     return {
       skills: {
         backlog: [
-          {name: "javascript", logoPath: "/src/assets/icons/Skills/js.png", textColor: "#F0DB4F", borderColor: "#F0DB4F"},
-          {name: "rust", logoPath: "/src/assets/icons/Skills/rust.svg", textColor: "#F65D00", borderColor: "#F65D00"},
-          {name: "c++", logoPath: "/src/assets/icons/Skills/c++.png", textColor: "#00599C", borderColor: "#00599C"},
+          {name: "javascript", logoPath: "/assets/Skills/js.png", textColor: "#F0DB4F", borderColor: "#F0DB4F"},
+          {name: "rust", logoPath: "/assets/Skills/rust.svg", textColor: "#F65D00", borderColor: "#F65D00"},
+          {name: "c++", logoPath: "/assets/Skills/c++.png", textColor: "#00599C", borderColor: "#00599C"},
           ],
           inProgress: [
-            {name: "vue", logoPath: "/src/assets/icons/Skills/vue.svg", textColor: "#41B883", borderColor: "#41B883"},
-            {name: "typescript", logoPath: "/src/assets/icons/Skills/ts.png", textColor: "#007ACC", borderColor: "#007ACC"},
+            {name: "vue", logoPath: "/assets/Skills/vue.svg", textColor: "#41B883", borderColor: "#41B883"},
+            {name: "typescript", logoPath: "/assets/Skills/ts.png", textColor: "#007ACC", borderColor: "#007ACC"},
           ],
           done: [
-          {name: "python", logoPath: "/src/assets/icons/Skills/py.png", textColor: "#FFD040", borderColor: "#366C9A"},
-          {name: "vim", logoPath: "/src/assets/icons/Skills/vim.svg", textColor: "#019733", borderColor: "#019733"},
-          {name: "html", logoPath: "/src/assets/icons/Skills/html.webp", textColor: "#E44D26", borderColor: "#E44D26"},
-          {name: "css", logoPath: "/src/assets/icons/Skills/css.svg", textColor: "#264DE4", borderColor: "#264DE4"},
-          {name: "linux", logoPath: "/src/assets/icons/Skills/linux.png", textColor: "#FCC624", borderColor: "#ffffff"},
-          {name: "arch", logoPath: "/src/assets/icons/Skills/arch.png", textColor: "#1793D1", borderColor: "#1793D1"},
-          {name: "figma", logoPath: "/src/assets/icons/Skills/figma.png", textColor: "#F24E1E", borderColor: "#F24E1E"},
-          {name: "git", logoPath: "/src/assets/icons/Skills/git.png", textColor: "#F05032", borderColor: "#F05032"},
-          {name: "github", logoPath: "/src/assets/icons/github.svg", textColor: "#ffffff", borderColor: "#ffffff"},
-          {name: "postgreSQL", logoPath: "/src/assets/icons/Skills/postgresql.png", textColor: "#336791", borderColor: "#336791"},
+          {name: "python", logoPath: "/assets/Skills/py.png", textColor: "#FFD040", borderColor: "#366C9A"},
+          {name: "vim", logoPath: "/assets/Skills/vim.svg", textColor: "#019733", borderColor: "#019733"},
+          {name: "html", logoPath: "/assets/Skills/html.webp", textColor: "#E44D26", borderColor: "#E44D26"},
+          {name: "css", logoPath: "/assets/Skills/css.svg", textColor: "#264DE4", borderColor: "#264DE4"},
+          {name: "linux", logoPath: "/assets/Skills/linux.png", textColor: "#FCC624", borderColor: "#ffffff"},
+          {name: "arch", logoPath: "/assets/Skills/arch.png", textColor: "#1793D1", borderColor: "#1793D1"},
+          {name: "figma", logoPath: "/assets/Skills/figma.png", textColor: "#F24E1E", borderColor: "#F24E1E"},
+          {name: "git", logoPath: "/assets/Skills/git.png", textColor: "#F05032", borderColor: "#F05032"},
+          {name: "github", logoPath: "/assets/github.svg", textColor: "#ffffff", borderColor: "#ffffff"},
+          {name: "postgreSQL", logoPath: "/assets/Skills/postgresql.png", textColor: "#336791", borderColor: "#336791"},
 
           
         ]
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  assetsInclude: ['**/*.svg'], // Ensure SVG icons are included
 })
